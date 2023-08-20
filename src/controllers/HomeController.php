ff14bca2686013dd22d049eeb02dd8e435168090
Joaov535 <joaovictorcruz535@gmail.com>
2023-08-19 21:35:18 -0300

    remaking project

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -7,18 +7,4 @@ use \core\Controller;
 class HomeController extends Controller
 {
 
-    public function index()
-    {
-        if (!isset($_SESSION['UserLogged'])) {
-            $this->render('home');
-        } else {
-
-            $this->redirect('/tasks');
-        }
-    }
-
-    public function teste()
-    {
-        $this->render('teste');
-    }
 }
