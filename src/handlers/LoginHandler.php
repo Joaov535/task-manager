afa9390ce5d36d70da4c14b67771b4e04e1c3e20
Joaov535 <joaovictorcruz535@gmail.com>
2023-08-20 00:49:29 -0300

    working on login

--- src/handlers/LoginHandler.php
+++ src/handlers/LoginHandler.php
@@ -1,2 +1,16 @@
 <?php 
 
+namespace src\handlers;
+
+class LoginHandler 
+{
+
+    public static function checkLogin($name, $pw)
+    {
+        if(!empty($_SESSION['token'])) {
+            $token = $_SESSION['token'];
+
+            
+        }
+    }
+}
--- src/models/User.php
+++ src/models/User.php
@@ -1,7 +1,52 @@
 <?php
 namespace src\models;
+
 use \core\Model;
 
 class User extends Model {
 
+    private $id;
+    private $userName;
+    private $userPassword;
+    private $token;
+
+    public function getUserId()
+    {
+        return $this->id;
+    }
+
+    public function setUserId($id)
+    {
+        $this->$id = $id;
+    }
+
+    public function getUserName()
+    {
+        return $this->userName;
+    }
+
+    public function setUserName($name)
+    {
+        $this->userName = $name;
+    }
+
+    public function getUserPassword()
+    {
+        return $this->userPassword;
+    }
+
+    public function setUserPassword($pw)
+    {
+        $this->userPassword = $pw;
+    }
+
+    public function getUserToken()
+    {
+        return $this->token;
+    }
+
+    public function setUsertoken($name, $email)
+    {
+        $this->token = md5(time().rand().$name.$email);
+    }
 }
