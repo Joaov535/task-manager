fe528b21264231856ada0c9cf5794ad111c8ed27
Joaov535 <joaovictorcruz535@gmail.com>
2023-08-20 14:24:11 -0300

    login

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -7,4 +7,15 @@ use \core\Controller;
 class LoginController extends Controller
 {
     
+    public function signin()
+    {
+        $this->render('login');
+    }
+
+    public function signinAction()
+    {
+        $userName = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+        $userPass = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    }
 }
--- src/handlers/LoginHandler.php
+++ src/handlers/LoginHandler.php
@@ -1,16 +1,28 @@
-<?php 
+<?php
 
 namespace src\handlers;
 
-class LoginHandler 
+use \src\models\User;
+
+class LoginHandler
 {
 
-    public static function checkLogin($name, $pw)
+    public static function checkLogin()
     {
-        if(!empty($_SESSION['token'])) {
+        if (!empty($_SESSION['token'])) {
+
             $token = $_SESSION['token'];
 
-            
+            $data = User::select()->where('token', $token)->one();
+
+            $userLogged = new User();
+            $userLogged->setUserId($data['id']);
+            $userLogged->setUserName($data['username']);
+            $userLogged->setUserPassword($data['userpassword']);
+
+            return $userLogged;
         }
+
+        return false;
     }
 }
